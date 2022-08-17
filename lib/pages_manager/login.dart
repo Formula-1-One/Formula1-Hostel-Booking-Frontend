4af26c60ca789a8392ebd2d7e1562d047c04b1d7
khaleb165 <calebagyei165@gmail.com>
2022-08-17 21:41:14 +0000

    first commit

---
+++ lib/pages_manager/login.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home:LoginPage() ,
+));
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
