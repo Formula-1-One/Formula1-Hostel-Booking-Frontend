9acb5c0eef72cb6635bc532ca71e97ac7cb70b5f
khaleb165 <calebagyei165@gmail.com>
2022-08-19 12:48:49 +0000

    manager update

---
+++ lib/pages_manager/manager_create_password.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home:ManagerForgotPassword() ,
+));
+
+class ManagerForgotPassword extends StatelessWidget {
+  const ManagerForgotPassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
