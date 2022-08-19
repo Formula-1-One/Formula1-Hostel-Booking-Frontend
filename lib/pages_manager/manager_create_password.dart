58de6271c8f453ed8abed736aba47c16c004bfb6
khaleb165 <calebagyei165@gmail.com>
2022-08-19 22:55:35 +0000

    manager update 6

--- lib/pages_manager/manager_create_password.dart
+++ lib/pages_manager/manager_create_password.dart
@@ -1,14 +1,25 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:hostel_booking_app_ui_f1/pages_manager/login.dart';
+import 'package:hostel_booking_app_ui_f1/pages_manager/manager_forgot_password.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
   home:ManagerCreatePassword() ,
 ));
 
-class ManagerCreatePassword extends StatelessWidget {
+class ManagerCreatePassword extends StatefulWidget {
   const ManagerCreatePassword({Key? key}) : super(key: key);
+
+  @override
+  State<ManagerCreatePassword> createState() => _ManagerCreatePasswordState();
+}
+
+class _ManagerCreatePasswordState extends State<ManagerCreatePassword> {
+  bool hide = true;
+  TextEditingController password = TextEditingController();
+  TextEditingController confirmPassword = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +66,77 @@ class ManagerCreatePassword extends StatelessWidget {
                 fontSize: 30
             ),
           ),
+              SizedBox(height: 40.0,),
+              TextField(
+                controller: password,
+                obscureText: hide,
+                decoration: InputDecoration(
+                    hintText: 'Password',
+                    suffixIcon: IconButton(
+                      onPressed: (){
+                        setState(() {
+                          hide= !hide;
+                        });
+                      },
+                      icon:hide? Icon(Icons.visibility_off):
+                      Icon(Icons.visibility),
+                    )
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              TextField(
+                controller: confirmPassword,
+                obscureText: hide,
+                decoration: InputDecoration(
+                    hintText: 'Confirm Password',
+                    suffixIcon: IconButton(
+                      onPressed: (){
+                        setState(() {
+                          hide= !hide;
+                        });
+                      },
+                      icon:hide? Icon(Icons.visibility_off):
+                      Icon(Icons.visibility),
+                    )
+                ),
+              ),
+              SizedBox(height: 10.0,),
+
+              TextButton(
+                onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ManagerForgotPassword()));
+              },
+                child: Text(
+                  "back",
+                  style: TextStyle(fontSize: 15,
+                  color: Colors.blue),
+                ),),
+              SizedBox(height: 20.0,),
+              Center(
+                child: ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.deepPurpleAccent,
+                    padding: EdgeInsets.symmetric(horizontal: 55,vertical: 20),
+                  ),
+                  onPressed: (){
+                    if(password.text != confirmPassword.text){
+                      showDialog(context: context, builder: (context){
+                        return AlertDialog(
+                          title: Text('Alert!!!'),
+                          content: Text('Passwords do not match'),
+                        );
+                      }
+                      );}
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => ManagerLoginPage()));
+                  },
+                  child: Text(
+                    'Done',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),),
+              ),
         ])
         ),
 
