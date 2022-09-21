1c201810bfb6ed366eb1623797c1541ad8df93cb
eoadanso <aericdanso98@gmail.com>
2022-09-21 12:22:05 +0000

    Update 7

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hostel Booking ',
+      title: 'F1 Hostel Booking ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
