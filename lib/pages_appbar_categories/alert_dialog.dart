fd492bec8be8a3af25e58c8f1ce4fb114b3c1163
khaleb165 <calebagyei165@gmail.com>
2022-09-28 23:07:34 +0000

    changes 3

--- lib/pages_appbar_categories/alert_dialog.dart
+++ lib/pages_appbar_categories/alert_dialog.dart
@@ -16,27 +16,24 @@ class AlertDialogs{
         builder: (BuildContext context){
       return AlertDialog(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          title:  Text("Formula 1", style:GoogleFonts.poppins(
-            textStyle: TextStyle(
-            fontSize: 20,
-          ),),),
           content:Text("Are you sure you want to log out? ", style:GoogleFonts.poppins(
           textStyle:
           TextStyle(
             fontSize: 16,
+            fontWeight: FontWeight.bold
           ),),),
           actions: <Widget>[
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                SizedBox(width: 130,),
                 GestureDetector(
                     onTap: () => {Navigator.of(context).pop(DialogAction.cancel)},
                     child:Text("CANCEL",style:GoogleFonts.poppins(
                      textStyle: TextStyle(
-                      fontSize: 20,
+                      fontSize: 15,
                     ),),)
                 ),
-
+                  SizedBox(width: 30,),
                 GestureDetector(
                     onTap: () => {Navigator.of(context).pop(DialogAction.yes)},
                     child: InkWell(
@@ -45,7 +42,7 @@ class AlertDialogs{
                       child:Text("LOG OUT",style: GoogleFonts.poppins(
                         textStyle:
                       TextStyle(
-                          fontSize: 20,
+                          fontSize: 15,
                           color: Colors.red
                       ),),)
                     ))
